5b6bd6652b235e7546187c8d0a9362643311a62e
md5projectx <md5projectx@gmail.com>
2026-07-20 02:00:38 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,43 @@
-const CACHE='abu-hamoudi-menu-v3-inline-assets';
-const APP=['./','./index.html','./menu.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./assets/logo.png','./assets/hero-food.jpg'];
+const CACHE_NAME = "abu-hamoudi-menu-v1";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./menu.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting()));
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET')return;
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
-      return response;
-    }).catch(()=>caches.match('./menu.html')))
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
   );
 });
